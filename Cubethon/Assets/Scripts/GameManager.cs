0fe3244305cc28e456735ac1195b9c95fd420daa
ebtesam25 <ebtesam@iut-dhaka.edu>
2019-06-16 15:32:07 +0600

    Cubethon

---
+++ Cubethon/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+	bool GameHasEnded=false;
+	public float RestartDelay=1f;
+	public GameObject completeLevelUI;
+
+	public void CompleteLevel(){		
+		completeLevelUI.SetActive(true);
+	}
+
+	public void EndGame(){
+		if (GameHasEnded == false) {
+			GameHasEnded = true;
+			Debug.Log ("GAME OVER");
+			Invoke ("Restart", RestartDelay);
+		}
+	
+	}
+	void Restart(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		
+	}
+}
